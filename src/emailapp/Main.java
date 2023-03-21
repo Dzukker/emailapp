8487e666ce7270fd0ceb41a40478a4f603b7af65
Dzukker <jakub.zukowski112@gmail.com>
2023-03-21 14:31:21 +0100

    added more code

--- src/emailapp/Main.java
+++ src/emailapp/Main.java
@@ -4,7 +4,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-//    asa
+        System.out.println("Input first name and last name:");
+        String firstname = scanner.next();
+        String lastname = scanner.next();
+
+        System.out.println("Input the department name");
+        String department = scanner.next();
+
+        System.out.print(getEmail(firstname, lastname, department));
+    }
+
+    public static String getEmail(String firstname, String lastname, String department) {
+
+        if (!department.equals(null)) {
+            String email = firstname + "." + lastname + "@" + department + ".company.com";
+            return email;
+        }
+        String email = firstname + "." + lastname + "@company.com";
+        return email;
     }
 }
